a4d3e52d4efaf8c37d902a462223cae274086e3a
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-06-29 04:12:44 +0000

    Automatically backed up by Learn

--- src/containers/BandsContainer.js
+++ src/containers/BandsContainer.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import BandInput from '../components/BandInput';
 
 class BandsContainer extends Component {
   render() {
