b37b43bce3f544c743f260e4c71929a90b920fdb
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-06-29 07:12:21 +0000

    Automatically backed up by Learn

--- src/containers/BandsContainer.js
+++ src/containers/BandsContainer.js
@@ -3,21 +3,21 @@ import { connect } from 'react-redux';
 import BandInput from '../components/BandInput';
 
 class BandsContainer extends Component {
- renderBands = () => this.props.bands.map((band, index) => <li>{band.name}</li>)
- 
- render() {
+
+  renderBands = () => this.props.bands.map((band, index) => <li>{band.name}</li>)
+
+  render() {
     return(
       <div>
-        BandInput addBand={this.props.addBand}/>
+        <BandInput addBand={this.props.addBand}/>
         {this.renderBands()}
- 
       </div>
-    )
+    );
   }
-}
+};
 
 const mapStateToProps = state => {
-  return{
+  return {
     bands: state.bands
   }
 }
